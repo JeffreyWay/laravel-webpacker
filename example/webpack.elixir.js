bebb57c8abc29da07d59ba34064ee2340f1bc0c6
Jeffrey Way <jeffrey@jeffrey-way.com>
2016-12-14 21:34:46 -0500

    Show full API

--- example/webpack.elixir.js
+++ example/webpack.elixir.js
@@ -1,6 +1,18 @@
 let Elixir = require('laravel-webpacker');
 
-Elixir.js('./resources/assets/js/app.js', './public/js/bundle.js', { vendor: ['vue', 'jquery'] })
+Elixir.js('./resources/assets/js/app.js', './public/js/bundle.js')
       .sass('./resources/assets/sass/app.scss', './public/css/complete.css');
+
+// Full API
+// Elixir.js(src, output);
+// Elixir.extract(vendorLibs);
+// Elixir.sass(src, output);
+// Elixir.less(src, output);
+// Elixir.combine(files, destination);
+// Elixir.copy(from, to);
+// Elixir.minify(file);
+// Elixir.sourceMaps(); // Enable sourcemaps
+// Elixir.version(); // Enable hashing.
+// Elixir.disableNotifications();
 
 module.exports = Elixir;
